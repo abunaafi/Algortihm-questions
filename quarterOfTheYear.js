a5a3f2f2441e026e830ba2d6842421c654073de7
aabuNaafi <abdisalanyusuf10@gmail.com>
2022-01-26 10:24:57 +0000

    update

---
+++ quarterOfTheYear.js
@@ -0,0 +1,26 @@
+const quarterOf = (month) => {
+    // Your code here
+   switch(month){
+        case 1:
+       case 2:
+       case 3:
+       return 1;
+       break;
+       case 4:
+       case 5:
+       case 6:
+       return 2;
+       break;
+       case 7:
+       case 8:
+       case 9:
+       return 3;
+       break;
+     case 10:
+       case 11:
+       case 12:
+       return 4;
+       break;
+       
+   }
+  };
