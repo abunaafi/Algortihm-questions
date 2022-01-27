267a1a26764de94fa0c911a40babbfb981cfad40
aabuNaafi <abdisalanyusuf10@gmail.com>
2022-01-27 11:14:10 +0000

    update

---
+++ dogYears.js
@@ -0,0 +1,14 @@
+let myAge = 35;
+//the next line will be changed 
+let earlyYears =2;
+earlyYears*=10.5;
+let laterYears = myAge-2;
+//this is the later dog years 
+laterYears*=4;
+console.log(laterYears);
+console.log(earlyYears);
+// this is my age in dog years
+let myAgeInDogYears= earlyYears+laterYears;
+let myName ="abuNaafi";
+console.log(myName.toLowerCase());
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
